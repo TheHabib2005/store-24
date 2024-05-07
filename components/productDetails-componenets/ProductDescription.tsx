7afa59c35822901b24643950ca381c8a6434aa51
habib <mdwear2005@gmail.com>
2024-05-07 22:40:56 +0600

    j

--- components/productDetails-componenets/ProductDescription.tsx
+++ components/productDetails-componenets/ProductDescription.tsx
@@ -10,7 +10,7 @@ const ProductDescription = () => {
                 <h6 className="text-[18px] font-medium text-qblack mb-2">Introduction</h6>
                 <p className="text-[15px] text-qgray text-normal mb-10">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the
+                    Lorem Ipsum has been the  standard dummy text ever since the
                     1500s, when an unknown printer took a galley of type and scrambled it to
                     make a type specimen book. It has survived not only five centuries but
                     also the on leap into electronic typesetting, remaining essentially
